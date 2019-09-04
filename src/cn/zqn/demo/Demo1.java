312396429326acfc25c38b0703f77e39281c8419
ZhaoQiannan <364596817@qq.com>
2019-09-04 14:32:59 +0800

    第一次修改

--- src/cn/zqn/demo/Demo1.java
+++ src/cn/zqn/demo/Demo1.java
@@ -8,4 +8,8 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("第一个Git项目");
     }
+
+    public void test(){
+        System.out.println("第二次输出！");
+    }
 }
