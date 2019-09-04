962a910eb5634f55e6f4a520e7f6a3fb13c1986f
ZhaoQiannan <364596817@qq.com>
2019-09-04 16:10:32 +0800

    二次改变

--- src/cn/zqn/demo/Demo1.java
+++ src/cn/zqn/demo/Demo1.java
@@ -7,9 +7,10 @@ package cn.zqn.demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("第一个Git项目");
+        Demo1.test();
     }
 
-    public void test(){
+    public static void test(){
         System.out.println("第二次输出！");
     }
 }
